44da228bc52f6c9d87013782821cce9eed454c58
roy-santos <roysantos112@gmail.com>
2020-07-16 14:31:09 -0700

    Initial Heroku push

---
+++ app.py
@@ -0,0 +1,67 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+from pathlib import Path
+
+# Import fast.ai Library
+from fastai import *
+from fastai.vision import *
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+
+# Define a flask app
+app = Flask(__name__)
+
+path = Path("path")
+classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+# depricated use DataBunch.load_empty
+data2 = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+# learn = create_cnn(data2, models.resnet34)
+# learn.load('model_9086')
+path1 = Path("./path/models")
+learn = load_learner(path1, 'export.pkl')
+
+
+def model_predict(img_path):
+    """
+       model_predict will return the preprocessed image
+    """
+   
+    img = open_image(img_path)
+    pred_class,pred_idx,outputs = learn.predict(img)
+    return pred_class
+    
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path)
+        preds = str(preds)
+        return preds
+    return None
+
+
+if __name__ == '__main__':
+    
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    // Init
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result').hide();
+
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function () {
+        var upBtn = document.getElementById("upload-file");
+        upBtn.getElementsByClassName("upload-label")[0].style.margin = "0";
+        
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+
+        var Bin = document.getElementById("uinf");
+        Bin.getElementsByClassName("recycle-bin")[0].style.position = "relative";
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                $('#result').text(' Model predicts the garbage as:  ' + data);
+                console.log('Success!');
+            },
+        });
+    });
+
+});
